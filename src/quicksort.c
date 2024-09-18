9eab13a739e15c56d708ef076fca22a606bee0a1
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-19 01:18:23 +0900

    (da-raccoon): algorithm:quicksort: clean-up
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- src/quicksort.c
+++ src/quicksort.c
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: GPL-2.0+
 // Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
+#include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
 
 #include "common.h"
 
-static inline ssize_t __partition(void *base, size_t size,
+static inline ssize_t __partition(uint8_t *base, size_t size,
     int (*Compare)(const void *, const void *), ssize_t left, ssize_t right)
 {
     ssize_t first = left;
-    void *pivot = base + first * size;
+    uint8_t *pivot = &base[first * size];
 
     ++left;
     while (left <= right) {
-        while ((Compare(base + left * size, pivot) <= 0) && (left < right))
+        while ((Compare(&base[left * size], pivot) <= 0) && (left < right))
             ++left;
 
-        while ((Compare(base + right * size, pivot) > 0) && (left <= right))
+        while ((Compare(&base[right * size], pivot) > 0) && (left <= right))
             --right;
 
         if (left < right)
@@ -31,7 +32,7 @@ static inline ssize_t __partition(void *base, size_t size,
     return right;
 }
 
-static void __quickSort(void *base, size_t size,
+static void __quickSort(uint8_t *base, size_t size,
     int (*Compare)(const void *, const void *), ssize_t left, ssize_t right)
 {
     if (left < right) {
@@ -45,5 +46,5 @@ static void __quickSort(void *base, size_t size,
 void QuickSort(void *base, size_t nmemb, size_t size,
     int (*Compare)(const void *, const void *))
 {
-    __quickSort(base, size, Compare, 0, nmemb - 1);
+    __quickSort((uint8_t *)base, size, Compare, 0, nmemb - 1);
 }
