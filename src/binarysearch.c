c9e01c051f570102b4093231683fbcb476291ada
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-19 01:27:07 +0900

    (da-raccoon): algorithm:binarysearch: clean-up
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- src/binarysearch.c
+++ src/binarysearch.c
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0+
 // Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
 #include <algorithm/binarysearch.h>
 
-void *BinarySearch(const void *key, void *base, size_t nmemb, size_t size,
-    int (*Compare)(const void *, const void *))
+static inline void *__binarySearch(const void *key, const uint8_t *base,
+    size_t nmemb, size_t size, int (*Compare)(const void *, const void *))
 {
     ssize_t left, right;
 
@@ -16,10 +17,10 @@ void *BinarySearch(const void *key, void *base, size_t nmemb, size_t size,
 
     while (left <= right) {
         ssize_t mid = (left + right) / 2;
-        int diff = Compare(key, base + mid * size);
+        int diff = Compare(key, &base[mid * size]);
 
         if (diff == 0)
-            return base + mid * size;
+            return (void *)&base[mid * size];
         else if (diff > 0)
             left = mid + 1;
         else
@@ -27,4 +28,10 @@ void *BinarySearch(const void *key, void *base, size_t nmemb, size_t size,
     }
 
     return NULL;
+}
+
+void *BinarySearch(const void *key, const void *base, size_t nmemb, size_t size,
+    int (*Compare)(const void *, const void *))
+{
+    return __binarySearch(key, (const uint8_t *)base, nmemb, size, Compare);
 }
