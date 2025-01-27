2b236de5f2ad37c133beb036af7397cfca554521
YOUNGJIN JOO <neoelec@gmail.com>
2025-01-28 06:10:48 +0900

    (da-raccoon): swap for any data type
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- src/common.h
+++ src/common.h
@@ -12,10 +12,10 @@
 #define COMPARE(func, base, off_a, off_b, size) \
     func(BASE(base, off_a, size), BASE(base, off_b, size))
 
-static inline void b_swap(void *base, size_t off_a, size_t off_b, size_t size)
+static inline void swap(void *_a, void *_b, size_t size)
 {
-    uint8_t *a = BASE(base, off_a, size);
-    uint8_t *b = BASE(base, off_b, size);
+    uint8_t *a = _a;
+    uint8_t *b = _b;
     uint8_t tmp;
     size_t i;
 
@@ -24,6 +24,11 @@ static inline void b_swap(void *base, size_t off_a, size_t off_b, size_t size)
         *a = *b;
         *b = tmp;
     }
+}
+
+static inline void b_swap(void *base, size_t off_a, size_t off_b, size_t size)
+{
+    swap(BASE(base, off_a, size), BASE(base, off_b, size), size);
 }
 
 #endif /* __SRC_INTERNAL__COMMON_H__ */
